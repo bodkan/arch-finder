2102669bed047ddf4f084c70cafc400a9a6a6028
Martin Petr <git@bodkan.net>
2018-12-12 13:32:52 +0100

    Add filtering on derived Nea state using an outgroup

--- src/arch_finder.cpp
+++ src/arch_finder.cpp
@@ -4,7 +4,7 @@
 #include <boost/iostreams/filtering_streambuf.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
 
-
+const int GT_POS = 9;
 
 // Split a single VCF line into individual tab-separated elements.
 std::vector<std::string> split_line(std::string& line)
@@ -18,41 +18,44 @@ std::vector<std::string> split_line(std::string& line)
 }
 
 
-/* // Convert genotypes from a VCF format into an EIGENSTRAT format by applying */
-/* // a series of regex substitutions on each line as a whole. */
-/* int convert_gt(const std::string& s) */
-/* { */
-/*     std::string converted(s); */
 
-/*     converted = std::regex_replace(converted, std::regex("0\\|0"),     "0"); */
-/*     converted = std::regex_replace(converted, std::regex("0/0"),       "0"); */
-
-/*     converted = std::regex_replace(converted, std::regex("0\\|1"),     "1"); */
-/*     converted = std::regex_replace(converted, std::regex("1\\|0"),     "1"); */
-/*     converted = std::regex_replace(converted, std::regex("0/1"),       "1"); */
-/*     converted = std::regex_replace(converted, std::regex("1/0"),       "1"); */
-
-/*     converted = std::regex_replace(converted, std::regex("1\\|1"),     "2"); */
-/*     converted = std::regex_replace(converted, std::regex("1/1"),       "2"); */
-
-/*     return std::stoi(converted); */
-/* } */
-
-// Convert genotypes from a VCF format into an EIGENSTRAT format by applying
-// a series of regex substitutions on each line as a whole.
+// Convert VCF genotypes into ALT allele counts.
 int convert_gt(const std::string& s)
 {
-    if      (s == "0|0") { return 2; }
-    else if (s == "0/0") { return 2; }
+    if      (s == "0|0") { return 0; }
+    else if (s == "0/0") { return 0; }
 
     else if (s == "0|1") { return 1; }
     else if (s == "1|0") { return 1; }
     else if (s == "0/1") { return 1; }
     else if (s == "1/0") { return 1; }
 
-    else if (s == "1|1") { return 0; }
-    else                 { return 0; } // (s == "1/1")
+    else if (s == "1|1") { return 2; }
+    else if (s == "1/1") { return 2; }
+    else throw std::runtime_error(std::string("Unsupported genotype: ") + s);
 }
+
+
+
+bool missing_samples(auto& outgroups, auto& out_pos,
+                     auto& archaics, auto& arch_pos,
+                     auto& africans, auto& afr_pos) {
+    int missing = 0;
+    if (out_pos.size() != outgroups.size()) {
+        std::cerr << "Not all outgroup samples are present in the VCF.\n";
+        missing++;
+    }
+    if (arch_pos.size() != archaics.size()) {
+        std::cerr << "Not all archaics are present in the VCF.\n";
+        missing++;
+    }
+    if (afr_pos.size() != africans.size()) {
+        std::cerr << "Not all Africans are present in the VCF.\n";
+        missing++;
+    }
+    return missing > 0;
+}
+
 
 
 int main(int argc, char* argv[])
@@ -61,36 +64,34 @@ int main(int argc, char* argv[])
     std::vector<std::string> args(argv, argv + argc);
 
     // find positions of command-line arguments
-    int vcf_cli = 0, bed_cli = 0, arch_cli = 0, afr_cli = 0;
+    int vcf_cli = 0, bed_cli = 0, out_cli = 0, arch_cli = 0, afr_cli = 0;
     for (int i = 0; i < argc; i++) {
-        if (args[i] == "--vcf") {
-            vcf_cli = i + 1;
-        } else if (args[i] == "--bed") {
-            bed_cli = i + 1;
-        } else if (args[i] == "--archaics") {
-            arch_cli = i + 1;
-        } else if (args[i] == "--africans") {
-            afr_cli = i + 1;
-        } else
-            continue;
+        if      (args[i] == "--vcf")       vcf_cli = i + 1;
+        else if (args[i] == "--bed")       bed_cli = i + 1;
+        else if (args[i] == "--outgroups") out_cli = i + 1;
+        else if (args[i] == "--archaics")  arch_cli = i + 1;
+        else if (args[i] == "--africans")  afr_cli = i + 1;
+        else continue;
     }
 
-    // extract path to VCF and list of archaic and African individual names
-    std::string vcf_path(args[vcf_cli]);
-    std::string bed_path(args[bed_cli]);
-    std::vector<std::string> archaics, africans;
+    // extract path to VCF and lists of outgroup, archaic and African sample names
+    std::string vcf_path(args[vcf_cli]), bed_path(args[bed_cli]);
+    std::vector<std::string> outgroups, archaics, africans;
+    for (int i = out_cli; i < arch_cli - 1; i++) outgroups.push_back(args[i]);
     for (int i = arch_cli; i < afr_cli - 1; i++) archaics.push_back(args[i]);
-    for (int i = afr_cli; i < argc; i++) africans.push_back(args[i]);
+    for (int i = afr_cli; i < argc; i++)         africans.push_back(args[i]);
 
-    if (!vcf_cli || !bed_cli || !archaics.size() || !africans.size()) {
-        std::cerr << "Usage:\n\t./arch-finder --vcf <path to VCF> --archaics <archaic samples> --africans <African samples>\n\n\tArguments must be provided in the specified order.";
+    if (!vcf_cli || !bed_cli || !out_cli || !outgroups.size() || !archaics.size() || !africans.size()) {
+        std::cerr << "Usage:\n\t./arch-finder --vcf <path to VCF> --bed <path to BED> --outgroups <names> --archaics <names> --africans <names>\n\n\tArguments must be provided in the specified order.\n";
         return 0;
     }
 
-    /* std::cout << vcf_path << "\n"; */
-    /* std::cout << bed_path << "\n"; */
-    /* for (auto x : archaics) std::cout << x << "\n"; */
-    /* for (auto x : africans) std::cout << x << "\n"; */
+    std::clog << "Input VCF: " << vcf_path << "\n";
+    std::clog << "Output BED: " << bed_path << "\n";
+    std::clog << "outgroups: "; for (auto x : outgroups) std::clog << x << " ";
+    std::clog << "\narchaics: ";  for (auto x : archaics) std::clog << x << " ";
+    std::clog << "\nAfricans: ";  for (auto x : africans) std::clog << x << " ";
+    std::clog << "\n";
 
     std::unique_ptr<std::istream> vcf_file;
     if (!std::ifstream(vcf_path)) {
@@ -115,7 +116,7 @@ int main(int argc, char* argv[])
 
         if (line.find("#") == 0) {
             auto elems = split_line(line);
-            for (std::size_t i = 9; i < elems.size(); i++) {
+            for (std::size_t i = GT_POS; i < elems.size(); i++) {
                 inds.push_back(elems[i]);
             }
             break;
@@ -123,54 +124,58 @@ int main(int argc, char* argv[])
     }
 
     // extract positions of archaics and Africans in the GT fields
-    std::vector<int> arch_pos, afr_pos;
-    for (auto x : archaics) for (std::size_t i = 0; i < inds.size(); i++) if (x == inds[i]) arch_pos.push_back(i + 9);
-    for (auto x : africans) for (std::size_t i = 0; i < inds.size(); i++) if (x == inds[i]) afr_pos.push_back(i + 9);
+    std::vector<int> out_pos, arch_pos, afr_pos;
+    for (auto x : outgroups) for (std::size_t i = 0; i < inds.size(); i++) if (x == inds[i]) out_pos.push_back(i + GT_POS);
+    for (auto x : archaics) for (std::size_t i = 0; i < inds.size(); i++) if (x == inds[i]) arch_pos.push_back(i + GT_POS);
+    for (auto x : africans) for (std::size_t i = 0; i < inds.size(); i++) if (x == inds[i]) afr_pos.push_back(i + GT_POS);
 
-    if (arch_pos.size() != archaics.size()) {
-        std::cerr << "Not all Neandertals given on the command-line are present in the VCF.\n";
-        return 1;
-    }
-    if (afr_pos.size() != africans.size()) {
-        std::cerr << "Not all Africans given on the command-line are present in the VCF.\n";
-        return 1;
-    }
-
-    /* for (int i : arch_pos) std::cout << i << "\n"; */
-    /* for (int i : afr_pos) std::cout << i << "\n"; */
+    if (missing_samples(outgroups, out_pos, archaics, arch_pos, africans, afr_pos)) return 1;
 
     // process the genotype part of the VCF
-    std::vector<std::pair<std::string, long>> positions;
+    std::vector<std::tuple<std::string, long, double, double, double>> sites;
     for (long i = 0; std::getline(*vcf_file, line); i++) {
-        if (i % 10000 == 1) std::cout << i << " sites processed\r" << std::flush;
-
         auto elems = split_line(line);
 
         // keep only biallelic SNPs
-        if (elems[3].length() != 1 || elems[4].length() != 1 || elems[4] == ".") continue;
+        if (elems[3].length() != 1 || elems[4].length() != 1 || elems[4] == "." || elems[4] == "-") continue;
 
-        std::vector<int> arch_gt, afr_gt;
+        std::vector<int> out_gt, arch_gt, afr_gt;
+        for (int i : out_pos) if (elems[i] != "./." && elems[i] != ".|.") out_gt.push_back(convert_gt(elems[i]));
         for (int i : arch_pos) if (elems[i] != "./." && elems[i] != ".|.") arch_gt.push_back(convert_gt(elems[i]));
         for (int i : afr_pos) if (elems[i] != "./." && elems[i] != ".|.") afr_gt.push_back(convert_gt(elems[i]));
-        if (arch_gt.size() != arch_pos.size() || !afr_gt.size()) continue;
+        if (out_gt.size() != out_pos.size() || arch_gt.size() != arch_pos.size() || !afr_gt.size()) continue;
 
         // calculate allele frequencies
+        float out_freq = std::accumulate(out_gt.begin(), out_gt.end(), 0) / float(2 * out_gt.size());
         float arch_freq = std::accumulate(arch_gt.begin(), arch_gt.end(), 0) / float(2 * arch_gt.size());
         float afr_freq = std::accumulate(afr_gt.begin(), afr_gt.end(), 0) / float(2 * afr_gt.size());
 
-        if (arch_freq != 1 || afr_freq != 0) continue;
+        // keep only sites at which outgroups == Africans == 0 (reference state) and
+        // archaics are fixed for the derived state
+        if (out_freq != 0 || arch_freq != 1 || afr_freq != 0) continue;
 
-        positions.push_back(std::make_pair(elems[0], std::stoul(elems[1])));
+        sites.push_back(std::make_tuple(elems[0], std::stoul(elems[1]), out_freq, arch_freq, afr_freq));
 
-        /* std::cout << arch_freq << "\t" << afr_freq << "\t#\t"; */
+        /* std::cout << elems[0] << "\t" << std::stoul(elems[1]) - 1 << "\t" << std::stoul(elems[1]) << "\t" << */
+        /*              anc << "\t" << der << "\t" << */
+        /*              out_freq << "\t" << arch_freq << "\t" << afr_freq << "\n"; */
+
+        /* for (auto x : out_gt) std::cout << x << "\t"; */
         /* for (auto x : arch_gt) std::cout << x << "\t"; */
         /* for (auto x : afr_gt) std::cout << x << "\n"; */
+
+        /* if (sites.size() > 100) break; */
     }
 
     // write coordinates
     std::ofstream bed(bed_path);
-    for (const auto & pos : positions) {
-        bed << pos.first << "\t" << pos.second - 1 << "\t" << pos.second << "\n";
+    for (const auto & s : sites) {
+        bed << std::get<0>(s) << "\t" <<      // chrom
+               std::get<1>(s) - 1 << "\t" <<  // start
+               std::get<1>(s) << "\t" <<      // end
+               std::get<2>(s) << "\t" <<      // outgroup freq
+               std::get<3>(s) << "\t" <<      // archaic freq
+               std::get<4>(s) << "\n";        // African freq
     }
     bed.close();
 
